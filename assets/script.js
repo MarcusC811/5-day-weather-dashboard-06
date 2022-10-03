359f81c2c9dcc0726d22568918afd560dc2eaf9f
OVOMARK <mcncstatefan@icloud.com>
2022-10-03 17:22:25 -0400

    failed jquery attempt

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
+var cityInput = document.getElementById("cityInput");
+var availableTags = [
+    "Angier",
+    "Fuquay",
+    "Raleigh"
+  ];
+
+$('#cityInput').autocomplete({
+    source: availableTags
+});
 var APIKey = '8e7dfd8a11ead85b1484cbd7aad925cc';
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
-fetch(queryURL)
+
+
+// fetch(queryURL)
