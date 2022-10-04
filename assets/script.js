bc543e71d2928b6576989a99f0bfa945bcba47a7
OVOMARK <mcncstatefan@icloud.com>
2022-10-04 09:16:14 -0400

    dropdown working

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,12 @@ $('#cityInput').autocomplete({
     source: availableTags
 });
 var APIKey = '8e7dfd8a11ead85b1484cbd7aad925cc';
-var city;
+var city = 'Raleigh';
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var cityWeather = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 
+console.log(cityWeather);
 
-
-
-// fetch(queryURL)
+function getInfo() {
+  
+}
