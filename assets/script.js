8978daaefb88d0286ca9f5d956a123886370307f
OVOMARK <mcncstatefan@icloud.com>
2022-10-07 18:16:39 -0400

    js link bug

--- assets/script.js
+++ assets/script.js
@@ -15,13 +15,13 @@ var hisIndex = 0;
 var APIKey = '8e7dfd8a11ead85b1484cbd7aad925cc';
 var APIKey3 = '87c21d5655399232649628c513cca2e0';
 
-const cityWeather = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&cnt=3&appid=" + APIKey;
+const cityWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&cnt=3&appid=" + APIKey;
 const city = cityInput.value;
 
-var cityGeo = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value+ "&limit=5&appid=" + APIKey;
+var cityGeo = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value+ "&limit=5&appid=" + APIKey;
 
 function getInfo() {
-  fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=" + APIKey)
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=" + APIKey)
     .then(function (response) {
       if(!response.ok) {
         alert("Couldn't find that city, Please Try Again")
@@ -54,7 +54,7 @@ function displayWeather (responseCity, cityLat, cityLog) {
       var dateString0 = moment.unix(data.daily[0].dt).format("MM/DD/YYYY");
       var humPerc0 = data.daily[0].humidity;
       var wCode0 = data.daily[0].weather[0].icon;
-      $("#pic0").attr( "src", "https://openweathermap.org/img/wn/" + wCode0 + "@2x.png");
+      $("#pic0").attr( "src", "httpss://openweathermap.org/img/wn/" + wCode0 + "@2x.png");
       sameTempSpan.innerText = Day0Temp + '°F';
       cityFill.innerText = responseCity + " Wind: " + data.daily[0].wind_speed + "MPH - " + "Humidity: " + humPerc0 + "% " +  dateString0;
 
@@ -63,7 +63,7 @@ function displayWeather (responseCity, cityLat, cityLog) {
       var dateString1 = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
       var humPerc1 = data.daily[1].humidity;
       var wCode1 = data.daily[1].weather[0].icon;
-      $("#pic1").attr( "src", "https://openweathermap.org/img/wn/" + wCode1 + "@2x.png");
+      $("#pic1").attr( "src", "httpss://openweathermap.org/img/wn/" + wCode1 + "@2x.png");
       nextdaySpan.innerText = Day1Temp + '°F';
       cityFill1.innerText = "Wind: " + data.daily[1].wind_speed + "MPH - " + "Humidity: " + humPerc1 + "% " + dateString1;
 
@@ -72,7 +72,7 @@ function displayWeather (responseCity, cityLat, cityLog) {
       var dateString2 = moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
       var humPerc2 = data.daily[2].humidity;
       var wCode2 = data.daily[2].weather[0].icon;
-      $("#pic2").attr( "src", "https://openweathermap.org/img/wn/" + wCode2 + "@2x.png");
+      $("#pic2").attr( "src", "httpss://openweathermap.org/img/wn/" + wCode2 + "@2x.png");
       followdaySpan.innerText = Day2Temp + '°F';
       cityFill2.innerText = "Wind: " + data.daily[2].wind_speed + "MPH - " + "Humidity: " + humPerc2 + "% " + dateString2;
 
@@ -81,7 +81,7 @@ function displayWeather (responseCity, cityLat, cityLog) {
       var dateString3 = moment.unix(data.daily[3].dt).format("MM/DD/YYYY");
       var humPerc3 = data.daily[3].humidity;
       var wCode3 = data.daily[3].weather[0].icon;
-      $("#pic3").attr( "src", "https://openweathermap.org/img/wn/" + wCode3 + "@2x.png");
+      $("#pic3").attr( "src", "httpss://openweathermap.org/img/wn/" + wCode3 + "@2x.png");
       followdaySpan2.innerText = Day3Temp + '°F';
       cityFill3.innerText = "Wind: " + data.daily[3].wind_speed + "MPH - " + "Humidity: " + humPerc3 + "% " + dateString3;
 
@@ -90,7 +90,7 @@ function displayWeather (responseCity, cityLat, cityLog) {
       var dateString4 = moment.unix(data.daily[4].dt).format("MM/DD/YYYY");
       var humPerc4 = data.daily[4].humidity;
       var wCode4 = data.daily[4].weather[0].icon;
-      $("#pic4").attr( "src", "https://openweathermap.org/img/wn/" + wCode4 + "@2x.png");
+      $("#pic4").attr( "src", "httpss://openweathermap.org/img/wn/" + wCode4 + "@2x.png");
       followdaySpan3.innerText = Day4Temp + '°F';
       cityFill4.innerText = "Wind: " + data.daily[4].wind_speed + "MPH - " + "Humidity: " + humPerc4 + "% " + dateString4;
       $(".hisBtn").on("click", test);
@@ -108,4 +108,4 @@ function hisStore (hisIndex, responseCity) {
 
 searchBtn.addEventListener("click", getInfo)
 
-// "https://openweathermap.org/img/wn/" + wCode0 + "@2x.png";
+// "httpss://openweathermap.org/img/wn/" + wCode0 + "@2x.png";
